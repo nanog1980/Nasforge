327b226bfe8013b54c042465cbf015555c403c2f
nanog1980 <id+nanog1980@users.noreply.github.com>
2026-07-06 21:29:52 +0200

    CSP: restreindre img-src au host Amazon au lieu du wildcard https:

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https:;
+    img-src 'self' blob: data: https://m.media-amazon.com;
     font-src 'self' data:;
     connect-src 'self' https://api.web3forms.com;
     frame-ancestors 'self';
